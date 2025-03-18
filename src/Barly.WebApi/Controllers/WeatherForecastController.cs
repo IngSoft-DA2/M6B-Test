a6d4c32da579537cbec0df2617fcdb85e3daa678
Max Halty <halty.maximo@principal.com>
2025-03-17 23:09:17 -0300

    fix alerts

--- src/Barly.WebApi/Controllers/WeatherForecastController.cs
+++ src/Barly.WebApi/Controllers/WeatherForecastController.cs
@@ -4,19 +4,10 @@ namespace Barly.WebApi.Controllers;
 
 [ApiController]
 [Route("[controller]")]
-public class WeatherForecastController : ControllerBase
+public class WeatherForecastController() : ControllerBase
 {
-    private static readonly string[] Summaries = new[]
-    {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
-
-    private readonly ILogger<WeatherForecastController> _logger;
-
-    public WeatherForecastController(ILogger<WeatherForecastController> logger)
-    {
-        _logger = logger;
-    }
+    private static readonly string[] Summaries =
+    ["Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"];
 
     [HttpGet(Name = "GetWeatherForecast")]
     public IEnumerable<WeatherForecast> Get()
