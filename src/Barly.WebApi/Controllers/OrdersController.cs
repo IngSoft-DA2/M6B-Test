f9a2bf5b0fd28b773eef148176e0024eecb8b301
Max Halty <maximohalty@gmail.com>
2025-03-24 14:05:25 -0300

    change response

--- src/Barly.WebApi/Controllers/OrdersController.cs
+++ src/Barly.WebApi/Controllers/OrdersController.cs
@@ -9,6 +9,6 @@ public class OrdersController(IOrderService orderService) : ControllerBase
     [HttpGet(Name = "GetOrders")]
     public IActionResult Get()
     {
-        return Ok(orderService.GetOrders());
+        return NotFound(orderService.GetOrders());
     }
 }
